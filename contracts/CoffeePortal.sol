a0b2372d0527ebb7067a400ffc21bc1cdd548b22
JovenTung <joventung88@gmail.com>
2024-04-17 17:48:06 +1000

    Created Contract and JS

---
+++ contracts/CoffeePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "hardhat/console.sol";
+
+contract CoffeePortal{
+    
+    constructor() payable{
+        console.log("Smart Contract Test");
+    }
+}
