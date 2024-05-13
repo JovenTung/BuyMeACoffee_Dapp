8ddffc63761873f6df098baad41d667031cbacd2
JovenTung <joventung88@gmail.com>
2024-05-13 19:14:07 +0800

    Updated run.js and deploy.js to be able to get the address. Changed the .address to .target

--- test/run.js
+++ test/run.js
@@ -1,20 +1,21 @@
 const main = async () => {
-    // This will actually compile our contract and generate the necessary files we need to work with our contract under the artifacts directory.
-    const coffeeContractFactory = await hre.ethers.getContractFactory('CoffeePortal');
-    const coffeeContract = await coffeeContractFactory.deploy();
 
-    // Simply log the address of the deployed contract
-    console.log("Coffee Contract deployed to:", coffeeContract.address);
-};
-
-const runMain = async () => {
-    try {
+    //fetch the contract and deploy it
+      const coffeeContractFactory = await hre.ethers.getContractFactory('CoffeePortal');
+      const coffeeContract = await coffeeContractFactory.deploy();
+        
+     console.log("Coffee Contract deployed to:", coffeeContract.target); //changed the address to target as address was showing undefined
+    };
+    
+    const runMain = async () => {
+      try {
         await main();
         process.exit(0);
-    } catch (error) {
+      } catch (error) {
         console.log(error);
         process.exit(1);
-    }
-};
-
-runMain();
+      }
+    };
+    
+    runMain();
+    
